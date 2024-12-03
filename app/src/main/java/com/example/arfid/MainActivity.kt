0a224bc949aa85655288657356a25c9ddefc15aa
4a756c6961 <julia.jessing@iubh-fernstudium.de>
2024-12-03 20:18:10 +0100

    Carddesign

--- app/src/main/java/com/example/arfid/MainActivity.kt
+++ app/src/main/java/com/example/arfid/MainActivity.kt
@@ -7,11 +7,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.arfid.ui.theme.ARFIDTheme
+import androidx.compose.material3.Text as Text1
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.material3.CardDefaults
+import androidx.compose.ui.res.stringResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,24 +30,66 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+
                 }
             }
         }
     }
 }
-
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
+    Text1(
         text = "Hello $name!",
         modifier = modifier
     )
 }
 
+
+@Composable
+fun ElevatedCardKind() {
+    ElevatedCard(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
+        modifier = Modifier
+            .size(width = 240.dp, height = 100.dp)
+    ) {
+        Text1(
+            text = stringResource(R.string.informationen_f_r_kinder),
+            modifier = Modifier
+                .padding(16.dp),
+            textAlign = TextAlign.Center,
+        )
+
+
+    }
+}
+
+@Composable
+fun ElevatedCardEltern() {
+    ElevatedCard(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
+        modifier = Modifier
+            .size(width = 240.dp, height = 100.dp)
+    ) {
+        Text1(
+            text = stringResource(R.string.informationen_f_r_eltern),
+            modifier = Modifier
+                .padding(16.dp),
+            textAlign = TextAlign.Center,
+        )
+
+
+    }
+}
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+
+fun ElevatedCardPreview() {
     ARFIDTheme {
-        Greeting("Android")
+        ElevatedCardEltern()
+        ElevatedCardKind()
     }
 }

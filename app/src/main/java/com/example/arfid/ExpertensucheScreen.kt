a1a70021d1aea9175be5d8e9b84137f639e26a77
4a756c6961 <julia.jessing@iubh-fernstudium.de>
2024-12-31 14:10:54 +0100

    Expertensuche mit GoogleMaps

--- app/src/main/java/com/example/arfid/ExpertensucheScreen.kt
+++ app/src/main/java/com/example/arfid/ExpertensucheScreen.kt
@@ -1,24 +1,102 @@
 package com.example.arfid
 
-
-import androidx.compose.foundation.layout.Box
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults.textFieldColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.arfid.ui.theme.ARFIDTheme
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 
 
+class ExpertensucheScreenActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ExpertensucheScreen()
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ExpertensucheScreen() {
-    Box(modifier = Modifier
-        .fillMaxSize(),
-        contentAlignment = Alignment.Center){
-        Text(text = "Experten suchen",
-            fontFamily = FontFamily.SansSerif,
-            fontSize = 30.sp)
-    }
+    //Suchleiste
+    var searchQuery by remember { mutableStateOf(TextFieldValue("")) }
 
+    Column(modifier = Modifier.fillMaxSize()) {
+        // Suchleiste Textfeld
+        SearchBar(
+            query = searchQuery,
+            onQueryChanged = { searchQuery = it }
+        )
+
+
+
+        //Karte mit Marker
+        val cameraPositionState = rememberCameraPositionState {
+            position = CameraPosition.fromLatLngZoom(
+                LatLng(54.0886707, 12.1400211), // Koordinaten von Rostock
+                10f
+            )
+        }
+
+        GoogleMap(
+            cameraPositionState = cameraPositionState,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Marker(
+                state = MarkerState(position = LatLng(54.0886707, 12.1400211)),
+                title = "Marker in Rostock"
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchBar(query: TextFieldValue, onQueryChanged: (TextFieldValue) -> Unit) {
+    TextField(
+        value = query,
+        onValueChange = onQueryChanged,
+        placeholder = {
+            Text(text = "Suche Experten...")
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        colors = textFieldColors(
+                        focusedIndicatorColor = Color.Blue,
+            unfocusedIndicatorColor = Color.Gray
+        )
+    )
+}
+
+@Preview
+@Composable
+fun ExpertensucheScreenPreview() {
+    ARFIDTheme {
+        ExpertensucheScreen()
+    }
 }
